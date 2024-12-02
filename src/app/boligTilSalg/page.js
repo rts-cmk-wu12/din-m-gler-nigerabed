a8a62ff64bb1e643e7b74dd68f2db4b4fc4b5001
nigerabed <nigerabed@gmail.com>
2024-12-02 21:33:06 +0100

    search/filter by bolig type

--- src/app/boligTilSalg/page.js
+++ src/app/boligTilSalg/page.js
@@ -1,26 +1,40 @@
 import BoligCard from "@/components/BoligCard";
 import PageTitle from "@/components/PageTitle";
+import SearchByBoligType from "@/components/SearchByBoligType";
 import Link from "next/link";
 
-export default async function boligTilSalg() {
-  const response = await fetch("https://dinmaegler.onrender.com/homes");
+// async function fetchBolig(url) {
+//   const response = await fetch(url);
+//   return response.json();
+// }
+
+export default async function boligTilSalg({ searchParams }) {
+  const category = searchParams.type;
+
+  const url =
+    category && category != "All"
+      ? `https://dinmaegler.onrender.com/homes?type_eq=${category}`
+      : `https://dinmaegler.onrender.com/homes`;
+
+  const response = await fetch(url);
   const allBoligDataata = await response.json();
-  // console.log("Bolig Dataaaa", allBoligDataata);
+
+  //const allBoligDataata = await fetchBolig(url);
 
   return (
     <>
-     
       <PageTitle title={"Boliger til salg"} />
+
       <div className="px-[17em] mt-[3em]">
-        <h3 className="font-semibold mb-[1em]"><span className="border-b-[3px] border-[#162A41]">Søg</span> efter dit drømmehus</h3>
-        <label htmlFor="options" className="font-normal"> Ejendomstype</label>
-        <select id="options" name="options"  className="h-[2em] w-[20em] mt-[.7em] block bg-white border border-slate-300 rounded-[.2em] shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 ">
-          <option value="option1">Ejendomstype</option>
-          <option value="option2">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </select>
-        
+        <h3 className="font-semibold mb-[1em]">
+          <span className="border-b-[3px] border-[#162A41]">Søg</span> efter dit
+          drømmehus
+        </h3>
+        <label htmlFor="options" className="font-normal">
+          {" "}
+          Ejendomstype
+        </label>
+        <SearchByBoligType />
       </div>
       <div className="grid grid-cols-2 place-content-around px-[15em] pt-[2em]">
         {allBoligDataata.map((bolig) => (
---
+++ src/components/SearchByBoligType.js
@@ -0,0 +1,34 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
+export default function SearchByBoligType() {
+  const router = useRouter();
+  const [boligType, setBoligType] = useState("All");
+
+  function handleOnchange(e) {
+    setBoligType(e.target.value);
+    console.log("=======", e.target.value);
+  }
+
+  useEffect(() => {
+    router.push("boligTilSalg?type=" + boligType);
+  }, [boligType, router]);
+
+  return (
+    <select
+      id="options"
+      value={boligType}
+      name="options"
+      onChange={handleOnchange}
+      className="h-[2em] w-[20em] mt-[.7em] block bg-white border border-slate-300 rounded-[.2em] shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 "
+    >
+      <option value="All">All</option>
+      <option value="Ejerlejlighed">Ejerlejlighed</option>
+      <option value="Villa">Villa</option>
+      <option value="Landejendom">Landejendom</option>
+      <option value="Byhus">Byhus</option>
+    </select>
+  );
+}
